73d2f75d9cca49bab3a1cf96bf64b6746c58b949
Jorge Antunes <ask@stoikerty.com>
2016-12-12 20:15:34 +0000

    add yarn lockfile, implement initial API wrapper

--- src/client/views/features/Home.jsx
+++ src/client/views/features/Home.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { stringify } from 'query-string';
+
+import { fetchRequest } from 'src/client/utils';
 
 import s from './Home/_style.scss';
 
-const displayName = 'Home';
-const defaultProps = {};
-const propTypes = {};
+export default class Home extends Component {
+  static displayName = 'Home'
+  static defaultProps = {}
+  static propTypes = {}
 
-const component = () => (
-  <div className={s.home}>
-    {'Home Feature'}
-  </div>
-);
+  constructor(props) {
+    super(props);
 
-component.displayName = displayName;
-component.defaultProps = defaultProps;
-component.propTypes = propTypes;
-export default component;
+    this.state = {};
+
+    const headers = {
+      X_REQUESTED_WITH: 'XMLHttpRequest',
+      'Content-Type': 'application/json',
+    };
+
+    const itunesOptions = {
+      entity: 'software',
+      country: 'NL',
+      term: 'github',
+      limit: 1,
+      price: 0,
+    };
+
+    fetchRequest({
+      // url: `https://itunes.apple.com/search${'?term=jack+johnson'}`,
+      url: `/itunes-api/?${stringify(itunesOptions)}`,
+      options: {
+        method: 'GET',
+        headers,
+      },
+
+      onSuccess: ({ json }) => {
+        console.log(json);
+      },
+
+      onError: (err) => {
+        console.log(err);
+      },
+
+      onException: (exc) => {
+        console.log(exc);
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className={s.home}>
+        {'Home'}
+      </div>
+    );
+  }
+}
--- src/server/router.jsx
+++ src/server/router.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
+import searchitunes from 'searchitunes';
 
 import { useServerRendering } from 'src/settings';
 import routes from '../client/routes';
@@ -9,22 +10,35 @@ import routes from '../client/routes';
 // Note:
 //   Adapted from server-rendering example: https://github.com/rackt/react-router/blob/latest/docs/guides/advanced/ServerRendering.md
 export default (req, res) => {
-  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    } else if (renderProps) {
-      const reactHtml = useServerRendering ?
-        ReactDOM.renderToString(<RouterContext {...renderProps} />) : '';
+  if (req.url.startsWith('/itunes-api')) {
+    console.log(req);
+    const searchParams = req.query;
+    searchitunes(searchParams, (error, data) => {
+      if (error) {
+        res.status(400).json({ message: 'Something broke', error });
+        return;
+      }
+      // All good
+      res.status(200).json({ message: 'All good', data });
+    });
+  } else {
+    match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+      if (error) {
+        res.status(500).send(error.message);
+      } else if (redirectLocation) {
+        res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      } else if (renderProps) {
+        const reactHtml = useServerRendering ?
+          ReactDOM.renderToString(<RouterContext {...renderProps} />) : '';
 
-      // Render `layout`-template using Handlebars
-      res.status(200).render('layout', {
-        reactHtml,
-        isDev: global.isDev,
-      });
-    } else {
-      res.status(404).send('Not found');
-    }
-  });
+        // Render `layout`-template using Handlebars
+        res.status(200).render('layout', {
+          reactHtml,
+          isDev: global.isDev,
+        });
+      } else {
+        res.status(404).send('Not found');
+      }
+    });
+  }
 };
