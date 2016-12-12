b7921993f7661a36f677563601d31870d2f2ff2b
Jorge Antunes <ask@stoikerty.com>
2016-12-12 20:14:39 +0000

    add some settings to distinguish client & server

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,12 @@
-// Shared settings between client and server
+// Detect whether the app is being rendered on the client or on the server
+// eslint-disable-next-line no-undef
+const creatingBuild = typeof buildSettings !== typeof undefined;
+// eslint-disable-next-line no-undef, jsx-control-statements/jsx-jcs-no-undef
+const env = creatingBuild ? buildSettings.env : process.env;
 
 export const useServerRendering = false;
-export const removeMe = true;
+export const isServer = !creatingBuild && env.NODE_ENV !== 'test';
+export const isClient = !isServer;
+
+// When building static files, this can be set to false
+export const isDev = true;
