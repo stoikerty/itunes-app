783d43feb8a0719a596ab83f025debaf1bc0c793
Jorge Antunes <ask@stoikerty.com>
2016-12-12 22:46:27 +0000

    display some of the results

--- src/client/views/features/Home/ResultsList.jsx
+++ src/client/views/features/Home/ResultsList.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export default class ResultsList extends Component {
   static displayName = 'ResultsList'
   static defaultProps = {}
-  static propTypes = {}
+  static propTypes = {
+    results: PropTypes.array,
+  }
 
   constructor(props) {
     super(props);
@@ -12,9 +14,32 @@ export default class ResultsList extends Component {
   }
 
   render() {
+    const { results } = this.props;
+
     return (
       <div>
         {'ResultsList'}
+        {results.map(({ wrapperType, artworkUrl100, artistName, collectionName }, index) => (
+          <div key={index}>
+            <Choose>
+              <When condition={wrapperType === 'artist'}>
+                { 'Artist' }
+                { artistName }
+              </When>
+              <When condition={wrapperType === 'album'}>
+                { '' }
+              </When>
+              <When condition={wrapperType === 'song'}>
+                <img src={artworkUrl100} alt={collectionName} />
+                { artistName }
+              </When>
+              <Otherwise>
+                <img src={artworkUrl100} alt={collectionName} />
+                { artistName }
+              </Otherwise>
+            </Choose>
+          </div>
+        ))}
       </div>
     );
   }
